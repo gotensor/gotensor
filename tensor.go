9e9faf70ade9015eefdd380fd64943c6a64dff38
Dariush Abbasi <dariushem@yahoo.com>
2023-04-17 23:22:44 +0330

    add print output for tensor type

--- tensor.go
+++ tensor.go
@@ -1,5 +1,10 @@
 package tensor
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Tensor datastructor base model
 type Tensor struct {
 	Data  []interface{}
@@ -8,4 +13,21 @@ type Tensor struct {
 
 func NewTensor() Tensor {
 	return Tensor{}
+}
+
+func (t Tensor) String() string {
+	var s strings.Builder
+
+	// Print the shape
+	fmt.Fprintf(&s, "Shape: %v\n", t.Shape)
+
+	// Print the data
+	for i := 0; i < len(t.Data); i++ {
+		fmt.Fprintf(&s, "%v ", t.Data[i])
+		if ((i + 1) % t.Shape[len(t.Shape)-1].(int)) == 0 {
+			fmt.Fprintln(&s)
+		}
+	}
+
+	return s.String()
 }
